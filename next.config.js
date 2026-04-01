a775454a73ff16d5249fba9d7de41a6b6c9e1965
guoyanjun01 <guoyanjun01@users.noreply.github.com>
2026-04-01 13:04:41 +0800

    fix: remove static export

--- next.config.js
+++ next.config.js
@@ -1,7 +1,6 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
-  output: 'export',          // 关键：启用静态导出
   images: {
     unoptimized: true,       // 静态模式下必须关闭图片优化
   },
