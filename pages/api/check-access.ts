122c04bf4ad32542f6915d5c0ac0485a7c4728f0
guoyanjun01 <guoyanjun01@users.noreply.github.com>
2026-04-14 17:11:17 +0800

    fix api type error

--- pages/api/check-access.ts
+++ pages/api/check-access.ts
@@ -1,9 +1,13 @@
+import type { NextApiRequest, NextApiResponse } from "next";
+
 let paidUsers: string[] = [];
 
-export default function handler(req, res) {
+export default function handler(req: NextApiRequest, res: NextApiResponse) {
   const { userId } = req.query;
 
-  const hasAccess = paidUsers.includes(userId);
+  const id = Array.isArray(userId) ? userId[0] : userId;
+
+  const hasAccess = paidUsers.includes(id || "");
 
   res.status(200).json({ hasAccess });
 }
